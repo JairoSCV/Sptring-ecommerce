7ae998344355fac162c9c45531057902bb855db9
Samiro <jairocornejovega@gmail.com>
2022-11-20 20:00:30 -0500

    Implemntacion de funcionalidades de cargar imagenes en vista principal

--- src/main/java/com/proyecto/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/proyecto/ecommerce/controller/AdministradorController.java
@@ -1,14 +1,28 @@
 package com.proyecto.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.ecommerce.model.Producto;
+import com.proyecto.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
     
+    @Autowired
+    private IProductoService productoService;
+
     @RequestMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        List<Producto> productos=productoService.findAll();
+        model.addAttribute("productos", productos);
         return "administrador/home";
     }
+
+
 }
--- src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
@@ -52,15 +52,6 @@ public class ProductoController {
         if(producto.getId()==null){//Cuando se crea un producto
             String nombreImagen = uploadFileService.saveImage(file);
             producto.setImagen(nombreImagen);
-        }else{
-            if(file.isEmpty()){//editamos el producto pero no cambiamos la imagen
-                Producto producto2 = new Producto();
-                producto2=productoService.get(producto.getId()).get();
-                producto.setImagen(producto2.getImagen());
-            }else{
-                String nombreImagen = uploadFileService.saveImage(file);
-                producto.setImagen(nombreImagen);
-            }
         }
 
         productoService.saveProducto(producto);
@@ -79,13 +70,39 @@ public class ProductoController {
     }
 
     @PostMapping("/update")
-    public String update(Producto producto){
+    public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
+        Producto p = new Producto();
+        p=productoService.get(producto.getId()).get();
+        
+        if(file.isEmpty()){//editamos el producto pero no cambiamos la imagen
+            
+            producto.setImagen(p.getImagen());
+        }else{//cuando se edita tambien la imagen
+
+            //eliminar cuando no sea la IMG por defecto
+            if(!p.getImagen().equals("default.jpg")){
+                uploadFileService.deleteImage(p.getImagen());
+            }
+                
+            String nombreImagen = uploadFileService.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }
+        
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos/";
     }
 
     @RequestMapping("/eliminar/{id}")
     public String eliminar(@PathVariable Integer id){
+        Producto p = new Producto();
+        p=productoService.get(id).get();
+
+        //eliminar cuando no sea la IMG por defecto
+        if(!p.getImagen().equals("default.jpg")){
+            uploadFileService.deleteImage(p.getImagen());
+        }
+
         productoService.delete(id);
         return "redirect:/productos/";
     }
